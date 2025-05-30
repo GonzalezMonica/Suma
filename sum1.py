054750d62d4660fd86377c12a263c14fe6935b94
González García Mónica <monica.gonzalez.cbtis03@gmail.com>
2025-05-30 09:16:58 -0600

    Add files via upload

---
+++ sum1.py
@@ -0,0 +1,9 @@
+class Suma:
+  def __init__(self):
+    self.num1=int(input("Ingresa un numero: "))
+    self.num2=int(input("Ingresa otro numero: "))
+    self.suma=self.num1+self.num2
+    print("Tu resultado es: ", self.suma)
+s=Suma()
+    
+    
